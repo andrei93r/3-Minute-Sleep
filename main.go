052b2ce98593aa7de842658b191f2097dca36119
Andrei Purice <rm.andrei93@gmail.com>
2023-11-27 16:20:06 -0500

    Good Starting Point

--- main.go
+++ main.go
@@ -1,5 +1,49 @@
 package main
 
+import (
+	"3-Minute-Sleep/pkgs"
+	"fmt"
+	"log"
+	"time"
+)
+
+const (
+	CheckInterval = 60 * time.Second
+)
+
 func main() {
+	prevResults := [3]bool{true, true, true}
+
+	i := 0
+
+	for {
+
+		if i > len(prevResults)-1 {
+			i = 0
+		}
+
+		displayActive, err := pkgs.IsDisplaying()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		prevResults[i] = displayActive
+
+		if i == len(prevResults)-1 {
+
+			if prevResults[0] == false && prevResults[1] == false && prevResults[2] == false {
+				err := pkgs.SetSuspendState(pkgs.SLEEP_SLEEP, pkgs.CRITICAL)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+		}
+
+		fmt.Print(i, prevResults)
+		time.Sleep(CheckInterval)
+		i++
+
+	}
 
 }
---
+++ pkgs/winInteractions.go
@@ -0,0 +1,58 @@
+package pkgs
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+const (
+	SLEEP_IDLE           = "0"
+	SLEEP_SLEEP          = "1"
+	SLEEP_HIBERNATE      = "2"
+	WAKE_EVENTS_ENABLED  = "0"
+	WAKE_EVENTS_DISABLED = "1"
+	NOT_CRITICAL         = "0"
+	CRITICAL             = "1"
+)
+
+var (
+	user32           = syscall.NewLazyDLL("user32.dll")
+	getSystemMetrics = user32.NewProc("GetSystemMetrics")
+	powrprof         = syscall.NewLazyDLL("powrprof.dll")
+	setSuspendState  = powrprof.NewProc("SetSuspendState")
+)
+
+func SetSuspendState(forceSleep, critical string) error {
+	disableWakeEvent := WAKE_EVENTS_ENABLED
+	ret, _, err := setSuspendState.Call(
+		uintptr(len(forceSleep)),
+		uintptr(unsafe.Pointer(&forceSleep)),
+		uintptr(len(disableWakeEvent)),
+		uintptr(unsafe.Pointer(&disableWakeEvent)),
+		uintptr(len(critical)),
+		uintptr(unsafe.Pointer(&critical)),
+	)
+	if ret == 0 {
+		return err
+	}
+	return nil
+}
+
+func IsDisplaying() (bool, error) {
+	nrDisplays, err := getNumberOfDisplays()
+	if err != nil {
+		return false, err
+	}
+
+	if nrDisplays < 1 {
+		return false, nil
+	}
+
+	return true, nil
+
+}
+
+func getNumberOfDisplays() (int, error) {
+	numDisplays, _, _ := getSystemMetrics.Call(80)
+	return int(numDisplays), nil
+}
